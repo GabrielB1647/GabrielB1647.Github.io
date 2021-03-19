c42318a5adb96ad20a4faf3ed1f906dd11ceb081
GabrielB1647 <80472549+GabrielB1647@users.noreply.github.com>
2021-03-18 21:47:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var ball = document.querySelector('.ball'),
+    info = document.querySelector('.info'),
+    y = 1,
+    gravity = 1; 
+
+    setInterval(function(){
+
+        if(y > 420) gravity = -gravity;
+        gravity += 1; 
+        y += gravity;
+        ball.style.top = (y) + 'px';
+       // info.innerText = "Y : "+y+", G : "+ gravity;
+
+    }, 50);
